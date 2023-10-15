29fd556d9888755a492ddd73a28cceb5a04089f5
Ian He <39037239+ianhe8x@users.noreply.github.com>
2023-10-16 11:10:28 +1300

    update demo to use kepler network

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ const { deploymentHttpLink } = require('@subql/apollo-links');
 const gql = require('graphql-tag');
 
 const options = {
-  authUrl: 'https://kepler-auth.thechaindata.com', // this is for testnet, use https://kepler-auth.subquery.network for kepler
-  deploymentId: 'QmStgQRJVMGxj1LdzNirEcppPf7t8Zm4pgDkCqChqvrDKG',
+  authUrl: 'https://kepler-auth.subquery.network', // this is for testnet, use https://kepler-auth.subquery.network for kepler
+  deploymentId: 'QmQ77QHgkKa81cVXbiChfLCcivucQpVmbim8GnnXxzd2Lu',
   httpOptions: { fetchOptions: { timeout: 5000 } },
   logger: console, // or any other custom logger
+  // scoreStore: for web app pass a local storage store to keep indexer scores after page refresh, `createLocalStorageStore({ttl: 86_400_000})`
 };
 
 const { link, cleanup } = deploymentHttpLink(options);
@@ -33,7 +34,7 @@ async function main() {
         }
     `
     let res;
-    for (let i=0;i<3;i++) {
+    for (let i=0;i<10;i++) {
       res = await client.query({ query: metadataQuery });
     }
     console.log(`res: ${JSON.stringify(res,null,2)}`)
